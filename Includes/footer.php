e7edf5112a8fc4b4409049cef6b209615380d27d
GemmerHare <chrisesterhuyse19@gmail.com>
2026-04-14 10:49:23 +0200

    added header, footer and search.php

---
+++ Includes/footer.php
@@ -0,0 +1,7 @@
+    <footer class="footer">
+        <p>Trustees &copy; 2026 Trustees. The middle man you can trust.</p>
+        <p>All rights reserved.</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
